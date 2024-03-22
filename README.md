58b78687e7f79fb19d76bf3be729a5174b07450b
kakzumooto <kakzumooto22@gmail.com>
2024-03-21 21:42:08 -0600

    Modificación del archivo Readme

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # ConsumoDeAPIs
-![ConsumoDeApisPokemon](https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png)
-#### Esta práctica es para demostrar el consumo de APIs, en este caso se utilizó la página de https://pokeapi.co/ como ejemplo.
-
-
-##### se hizo uso de:
-
-* html
-* css
-* javaScript
-
+Consumo de API de pokemon
--- script.js
+++ script.js
@@ -1,55 +1,84 @@
-const botonesHeader = document.querySelectorAll(".btn-header");
+window.onload = llenarSelect
+
+function iniciar(){
+    let boton = document.getElementById("btnCargar")
+    boton = addEventListener("click",clickBoton)
+
+}
 
 async function recibirUrl(url){
     let response = await fetch(url)
     return response.json();
 }
 
-async function cargarPokemon(){
-
-    const divPokemon = document.getElementById("listaPokemon");
-    divPokemon.innerHTML = "";
-
-    for(let x=1; x<=151; x++)
-    { 
-        let json = await recibirUrl('https://pokeapi.co/api/v2/pokemon/' + x);
-
-    let nombre= json.name
-    let habilidad = json.abilities[0].ability.name
-    let hp = json.stats[0].base_stat
-    let attack = json.stats[1].base_stat
-    let defense = json.stats[2].base_stat
-    let sped = json.stats[5].base_stat
-    let id = json.id
-    let idComplemento
-
-    if(id.toString().length==1){
-        idComplemento=` 00${id}`
+async function llenarSelect(){
+    let selectPokemon = document.getElementById("selectPokemon")
+    let opciones = "";
+    for(let x=1; x<=150; x++)
+    {
+        let json= await recibirUrl('https://pokeapi.co/api/v2/pokemon/' + x)  
+        opciones += `<option>${json.name}</option>`;       
     }
-    if(id.toString().length==2){
-        idComplemento=` 0${id}`
-    }
-    if(id.toString().length==3){
-        idComplemento=` ${id}`
-    }
-    let tipos = json.types.map((type) => `<p class="${type.type.name} tipo"> ${type.type.name}</p>`)
-    tipos = tipos.join('')
-    let imagen = json.sprites.other["official-artwork"].front_default
-
-    const pokemon = document.createElement("div");
-    pokemon.classList.add("pokemon");
-
-    pokemon.innerHTML=`
-            <p class="idPokemon">#${idComplemento}</p><br> `+`
-            <p class="nombre-pokemon">${nombre}</p><br> `+` 
-            <div class="pokemon-imagen">
-            <img src=${imagen}>
-            </div>`+`
-            <div class="pokemon-tipos">
-            ${tipos}
-            </div>
-        `; 
-        divPokemon.appendChild(pokemon);
-    }   
+    selectPokemon.innerHTML= opciones
+   iniciar()
 }
-cargarPokemon()
+
+
+
+async function clickBoton () { 
+
+    let seleccionPokemon = document.getElementById("selectPokemon").value
+        console.log(seleccionPokemon)
+    
+    
+let json= await recibirUrl('https://pokeapi.co/api/v2/pokemon/'+ seleccionPokemon)
+let divPokemon = document.getElementById("imagenPokemon")
+let nombre= json.name
+let habilidad = json.abilities[0].ability.name
+let hp = json.stats[0].base_stat
+let attack = json.stats[1].base_stat
+let defense = json.stats[2].base_stat
+let sped = json.stats[5].base_stat
+
+let id = json.id
+let idComplemento
+
+if(id.toString().length==1){
+idComplemento=` 00${id}`
+}
+if(id.toString().length==2){
+idComplemento=` 0${id}`
+}
+if(id.toString().length==3){
+idComplemento=` ${id}`
+}
+
+
+let tipos = json.types.map((type) => `<p class="${type.type.name} tipo"> ${type.type.name}</p>`)
+tipos = tipos.join('')
+
+
+
+
+let imagen = json.sprites.other["official-artwork"].front_default
+
+
+divPokemon.innerHTML=` 
+                    <p>#${idComplemento}</p><br> `+`
+                    <p>Nombre: ${nombre}</p><br> `+` 
+                    <p>Habilidad: ${habilidad}</p><br>`+`
+                    <p>HP: ${hp}</p><br>`+`
+                    <p>attack: ${attack}</p><br>`+`
+                    <p>defense: ${defense}</p><br>`+`
+                    <p>sped: ${sped}</p><br>`+`
+                    <div class="pokemon-tipos">
+                    ${tipos}
+                    </div>
+                    <img src=${imagen}>
+                    `
+
+}
+
+
+
+
